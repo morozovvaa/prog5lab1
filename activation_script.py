5296ec82201d174e2d52b51caf80fdbcdab69bef
morozovvaa <124806173+morozovvaa@users.noreply.github.com>
2024-09-13 17:39:31 +0300

    Add files via upload

---
+++ activation_script.py
@@ -0,0 +1,56 @@
+import re
+import sys
+import requests
+import types
+
+def url_hook(some_str):
+    if not some_str.startswith(("http", "https")):
+        raise ImportError
+    try:
+        response = requests.get(some_str)
+        response.raise_for_status()
+        data = response.text
+        filenames = re.findall("[a-zA-Z_][a-zA-Z0-9_]*.py", data)
+        modnames = {name[:-3] for name in filenames}
+        return URLFinder(some_str, modnames)
+    except requests.exceptions.RequestException as e:
+        print(f"Ошибка при загрузке модуля с URL {some_str}: {e}")
+        return None
+
+sys.path_hooks.append(url_hook)  # Добавляем url_hook в sys.path_hooks
+
+from importlib.abc import PathEntryFinder
+from importlib.util import spec_from_loader
+
+
+class URLFinder(PathEntryFinder):
+    def __init__(self, url, available):
+        self.url = url
+        self.available = available
+
+    def find_spec(self, name, target=None):
+        if name in self.available:
+            origin = "{}/{}.py".format(self.url, name)
+            loader = URLLoader(origin)  # Передаем URL-адрес в URLLoader
+            return spec_from_loader(name, loader, origin=origin)
+
+        else:
+            return None
+
+
+class URLLoader:
+    def __init__(self, url):
+        self.url = url
+
+    def create_module(self, spec):  # Добавляем метод create_module
+        return types.ModuleType(spec.name)  # Создаем новый модуль
+
+    def exec_module(self, module):
+        try:
+            response = requests.get(self.url)
+            response.raise_for_status()
+            source = response.text
+            code = compile(source, self.url, mode="exec")
+            exec(code, module.__dict__)
+        except requests.exceptions.RequestException as e:
+            print(f"Ошибка при загрузке модуля с URL {self.url}: {e}")
