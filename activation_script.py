94e3495426e31d68c578c0aaf7ecd862cf011bab
morozovvaa <124806173+morozovvaa@users.noreply.github.com>
2024-09-13 18:16:29 +0300

    Update activation_script.py

--- activation_script.py
+++ activation_script.py
@@ -2,6 +2,7 @@ import re
 import sys
 import requests
 import types
+import time
 
 def url_hook(some_str):
     if not some_str.startswith(("http", "https")):
@@ -46,11 +47,21 @@ class URLLoader:
         return types.ModuleType(spec.name)  # Создаем новый модуль
 
     def exec_module(self, module):
-        try:
-            response = requests.get(self.url)
-            response.raise_for_status()
-            source = response.text
-            code = compile(source, self.url, mode="exec")
-            exec(code, module.__dict__)
-        except requests.exceptions.RequestException as e:
-            print(f"Ошибка при загрузке модуля с URL {self.url}: {e}")
+        max_retries = 3  # Максимальное количество попыток
+        retry_delay = 1  # Начальная задержка
+        for attempt in range(1, max_retries + 1):
+            try:
+                response = requests.get(self.url)
+                response.raise_for_status()
+                source = response.text
+                code = compile(source, self.url, mode="exec")
+                exec(code, module.__dict__)
+                return  # Успешная загрузка, выходим из цикла
+            except requests.exceptions.RequestException as e:
+                print(f"Ошибка при загрузке модуля с URL {self.url}: {e}")
+                print(f"Попытка {attempt} из {max_retries}.")
+                if attempt < max_retries:
+                    time.sleep(retry_delay)  # Экспоненциальная задержка
+                    retry_delay *= 2  # Удваиваем задержку для следующей попытки
+                else:
+                    print(f"Превышено количество попыток загрузки модуля.")
